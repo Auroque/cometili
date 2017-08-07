a31dd3c7901c08a785c106abedc54aec17e54429
Auroque <luizaugusto_sv@hotmail.com>
2017-08-06 23:15:51 -0300

    Agora sim Slide Pronto

--- wp-content/themes/cometili_theme/footer.php
+++ wp-content/themes/cometili_theme/footer.php
@@ -1,4 +1,5 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/js/owl.carousel.min.js"></script>
 	<?php wp_footer(); ?>
 </body>
 </html>
