66bc9368769fc87407114a2535ed3abc064b0279
Auroque <luizaugusto_sv@hotmail.com>
2017-08-07 14:18:44 -0300

    Novo header para home

--- wp-content/themes/cometili_theme/front-page.php
+++ wp-content/themes/cometili_theme/front-page.php
@@ -1,34 +1,28 @@
-<?php get_header(); ?>
-	<div class="section slider">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-				<?php 
-				$sections = new WP_Query(
-					array(
-						'post_type' => 'slider',
-						'ignore_sticky_posts' => true,
-						'showposts' => -1,
-						'orderby' => 'title',
-						'order' => 'ASC'
-						)
-				);
-				?>
-					<div class="slider-area">
-						<div class="owl-carousel owl-theme">
-								<?php if ($sections->have_posts()): ?>
-									<?php while ($sections->have_posts()) : $sections->the_post(); ?>
-									<div class="item">
-										<img title="image title" alt="thumb image" class="wp-post-image" 
+<?php get_header('home'); ?>
+<?php 
+$sections = new WP_Query(
+	array(
+		'post_type' => 'slider',
+		'ignore_sticky_posts' => true,
+		'showposts' => -1,
+		'orderby' => 'title',
+		'order' => 'ASC'
+		)
+);
+?>
+<div class="slider-container">
+	<div class="slider-area">
+		<div class="owl-carousel owl-theme">
+				<?php if ($sections->have_posts()): ?>
+					<?php while ($sections->have_posts()) : $sections->the_post(); ?>
+					<div class="item">
+						<img title="image title" alt="thumb image" class="wp-post-image" 
 	             src="<?=wp_get_attachment_url( get_post_thumbnail_id() ); ?>" style="width:100%; height:auto;">
-									</div>
-									<?php endwhile; ?>
-								<?php endif; ?>
-							<?php wp_reset_query(); ?>
-						</div>
 					</div>
-				</div>
-			</div>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			<?php wp_reset_query(); ?>
 		</div>
 	</div>
+</div>
 <?php get_footer(); ?>
---
+++ wp-content/themes/cometili_theme/header-home.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<title><?php bloginfo( 'name' ); ?> | <?php bloginfo( 'description' ); ?></title><!--Título da página-->
+	<meta charset="<?php bloginfo( 'charset' ); ?>" /><!--Dinamismo do charset UTF-8 automático-->
+	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">  
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="stylesheet" media="all" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.theme.default.min.css">
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+<body>
+	<div class="section navegacao2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<a href="<?php echo get_option('home') ?>">
+					<img class="logo" src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="Cometili Logo"></a>
+				</div>
+				<div class="col-md-9">
+					<div class="posicao">
+						<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+					</div>
+				</div>	
+				<div class="col-md-1 login">
+					<a class="btn btn-default" href="#" role="button">LOGIN</a>						
+				</div>
+			</div>
+		</div>
+	</div>
