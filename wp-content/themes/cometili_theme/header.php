03125f544866b79748d459701b192a3db412d7bb
Auroque <luizaugusto_sv@hotmail.com>
2017-08-06 22:13:22 -0300

    Tentativa de colocar slider falhou, Erro no Jquery, removido para proxima tentativa

--- wp-content/themes/cometili_theme/header.php
+++ wp-content/themes/cometili_theme/header.php
@@ -17,8 +17,6 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="stylesheet" media="all" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.theme.default.min.css">
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
--- wp-content/themes/cometili_theme/links/link-bootstrap.php
+++ wp-content/themes/cometili_theme/links/link-bootstrap.php
@@ -17,7 +17,7 @@ function theme_js() {
 	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '../bootstrap/js/bootstrap.js', array ( 'jquery' ), 1.1, true);
 	wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '../bootstrap/js/animation.js', array ( 'jquery' ), 1.1, true);
 	wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '../js/owl.carousel.min.js', array ( 'jquery' ), 1.1, true);
-	wp_enqueue_script( 'my_custom_js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', array ( 'jquery' ), 1.1, true);
+	wp_enqueue_script( 'my_custom_js', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', array ( 'jquery' ), 1.1, true);
 
 }
 
