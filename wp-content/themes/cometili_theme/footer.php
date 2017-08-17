ff78175ffe34d9efd014c516017a69508d6e3b1c
Auroque <luizaugusto_sv@hotmail.com>
2017-08-17 15:34:11 -0300

    Rodape No Lugar

--- wp-content/themes/cometili_theme/footer.php
+++ wp-content/themes/cometili_theme/footer.php
@@ -1,5 +1,14 @@
+		<div class="section rodape">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<a href="<?php echo get_option('home') ?>">
+					<img class="logo-rodape" src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="Cometili Logo"></a>
+				</div>
+			</div>
+		</div>
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="<?php bloginfo('template_directory'); ?>/js/owl.carousel.min.js"></script>
 	<?php wp_footer(); ?>
 </body>
 </html>
