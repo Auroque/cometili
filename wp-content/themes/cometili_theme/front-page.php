e7380e4911e21470c0eeaa75a25ebffab959febf
Auroque <luizaugusto_sv@hotmail.com>
2017-08-17 13:10:00 -0300

    Slider Pronto, Iniciando Post

--- wp-content/themes/cometili_theme/front-page.php
+++ wp-content/themes/cometili_theme/front-page.php
@@ -10,22 +10,30 @@ $sections = new WP_Query(
 		)
 );
 ?>
-<div class="slider-container">
-	<div class="slider-area">
-		<div class="owl-carousel owl-theme">
-				<?php if ($sections->have_posts()): ?>
-					<?php while ($sections->have_posts()) : $sections->the_post(); ?>
-					<div class="item">
-						<img alt="thumb image" class="wp-post-image" src="<?=wp_get_attachment_url( get_post_thumbnail_id() ); ?>" style="width:100%; height:auto;">
-						<h1 class="slide-title"><?php the_title(); ?></h1>
-					</div>
-					<?php endwhile; ?>
-				<?php endif; ?>
-			<?php wp_reset_query(); ?>
+<div class="slider">
+    <ul class="slides">
+    <?php if ($sections->have_posts()): ?>
+		<?php while ($sections->have_posts()) : $sections->the_post(); ?>
+	      <li>
+	        <img alt="thumb image" class="wp-post-image" src="<?=wp_get_attachment_url( get_post_thumbnail_id() ); ?>" style="width:100%; height:650px;">
+	        <div class="caption center-align">
+	          <h3><?php the_title(); ?></h3>
+	          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+	        </div>
+	      </li>
+		<?php endwhile; ?>
+		<?php endif; ?>
+	<?php wp_reset_query(); ?>
+    </ul>
+  </div>
+<div class="section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<img class="projeto" src="<?php bloginfo('template_directory'); ?>/img/projeto.jpg">				
+			</div>
 		</div>
 	</div>
 </div>
-<div class="projeto">
-	<img src="<?php bloginfo('template_directory'); ?>/img/projeto.jpg">
-</div>
+
 <?php get_footer(); ?>
--- wp-content/themes/cometili_theme/links/link-bootstrap.php
+++ wp-content/themes/cometili_theme/links/link-bootstrap.php
@@ -2,6 +2,7 @@
 
 function theme_styles() {
 
+	wp_enqueue_style( 'materialize', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css' );
 	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '../bootstrap/css/bootstrap.min.css' );
 	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '../bootstrap/css/bootstrap.css' );
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
@@ -13,6 +14,7 @@ function theme_js() {
 
 	global $wp_scripts;
 
+	wp_enqueue_script( 'materialize_js', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js', array ( 'jquery' ), 1.1, true);
 	wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '../bootstrap/js/animation.js', array ( 'jquery' ), 1.1, true);
 	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '../bootstrap/js/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
 
