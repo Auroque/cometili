37f330b3bec9bccf8d8d0c7188722e01a4b95058
Auroque <luizaugusto_sv@hotmail.com>
2017-08-17 17:31:28 -0300

    Home Pronta

--- wp-content/themes/cometili_theme/footer.php
+++ wp-content/themes/cometili_theme/footer.php
@@ -5,6 +5,15 @@
 					<a href="<?php echo get_option('home') ?>">
 					<img class="logo-rodape" src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="Cometili Logo"></a>
 				</div>
+				<div class="col-md-3 col-md-offset-7">
+					<div class="sep-social">
+						<ul class="social">
+							<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/youtube.png" target="_blank"></a></li>
+							<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/instagram.png" target="_blank"></a></li>
+							<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/facebook.png" target="_blank"></a></li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
